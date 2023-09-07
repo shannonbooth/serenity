00cfdc858e426dfd836d79b07cd4d7ea5d7b2dc8
Shannon Booth <shannon@serenityos.org>
2023-09-07 21:36:05 +1200

    LibWeb: Port CharacterData from DeprecatedString to String
    The existing implementation has some pre-existing issues where it is
    incorrectly assumes that byte offsets are given through the IDL instead
    of UTF-16 code units. While making these changes, leave some FIXMEs for
    that.

--- Userland/Libraries/LibWeb/HTML/HTMLInputElement.cpp
+++ Userland/Libraries/LibWeb/HTML/HTMLInputElement.cpp
@@ -288,7 +288,7 @@ WebIDL::ExceptionOr<void> HTMLInputElement::run_input_activation_behavior()
 void HTMLInputElement::did_edit_text_node(Badge<BrowsingContext>)
 {
     // An input element's dirty value flag must be set to true whenever the user interacts with the control in a way that changes the value.
-    m_value = value_sanitization_algorithm(m_text_node->data());
+    m_value = value_sanitization_algorithm(m_text_node->data().to_deprecated_string());
     m_dirty_value = true;
 
     update_placeholder_visibility();
@@ -364,7 +364,7 @@ WebIDL::ExceptionOr<void> HTMLInputElement::set_value(String const& value)
     //    and the element has a text entry cursor position, move the text entry cursor position to the end of the
     //    text control, unselecting any selected text and resetting the selection direction to "none".
     if (m_text_node && (m_value != old_value)) {
-        m_text_node->set_data(m_value);
+        m_text_node->set_data(MUST(String::from_deprecated_string(m_value)));
         update_placeholder_visibility();
     }
 
@@ -503,7 +503,7 @@ void HTMLInputElement::create_shadow_tree_if_needed()
     MUST(m_placeholder_element->style_for_bindings()->set_property(CSS::PropertyID::Height, "1lh"sv));
 
     m_placeholder_text_node = heap().allocate<DOM::Text>(realm(), document(), MUST(String::from_deprecated_string(initial_value)));
-    m_placeholder_text_node->set_data(deprecated_attribute(HTML::AttributeNames::placeholder));
+    m_placeholder_text_node->set_data(attribute(HTML::AttributeNames::placeholder).value_or(String {}));
     m_placeholder_text_node->set_owner_input_element({}, *this);
     MUST(m_placeholder_element->append_child(*m_placeholder_text_node));
     MUST(element->append_child(*m_placeholder_element));
@@ -582,7 +582,7 @@ void HTMLInputElement::attribute_changed(DeprecatedFlyString const& name, Deprec
         }
     } else if (name == HTML::AttributeNames::placeholder) {
         if (m_placeholder_text_node)
-            m_placeholder_text_node->set_data(value);
+            m_placeholder_text_node->set_data(MUST(String::from_deprecated_string(value)));
     } else if (name == HTML::AttributeNames::readonly) {
         handle_readonly_attribute(value);
     }
@@ -910,7 +910,7 @@ void HTMLInputElement::reset_algorithm()
     // and then invoke the value sanitization algorithm, if the type attribute's current state defines one.
     m_value = value_sanitization_algorithm(m_value);
     if (m_text_node) {
-        m_text_node->set_data(m_value);
+        m_text_node->set_data(MUST(String::from_deprecated_string(m_value)));
         update_placeholder_visibility();
     }
 }
