998aace54605081e20a57ecfc6efee4580330497
Shannon Booth <shannon@serenityos.org>
2023-12-05 09:51:14 +1300

    LibWeb: Port SVGUseElement from DeprecatedString

--- Userland/Libraries/LibWeb/SVG/SVGUseElement.h
+++ Userland/Libraries/LibWeb/SVG/SVGUseElement.h
@@ -48,7 +48,7 @@ private:
 
     virtual JS::GCPtr<Layout::Node> create_layout_node(NonnullRefPtr<CSS::StyleProperties>) override;
 
-    Optional<FlyString> parse_id_from_href(DeprecatedString const& href);
+    static Optional<FlyString> parse_id_from_href(StringView href);
 
     JS::GCPtr<DOM::Element> referenced_element();
 
